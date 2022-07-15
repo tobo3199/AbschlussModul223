bafce487a4ac56fbf08bc174e1847a578e57d134
tobo3199 <tobias.sauter.003@outlook.com>
2022-07-15 16:04:19 +0200

    sql script erweitert

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,16 @@
 INSERT INTO fahrrad_type (id, typ) VALUES
-(1, 'bike');
+(1, 'bike'),
+(2, 'citybike');
 
 INSERT INTO marke (id, marken_type) VALUES
-(1, 'bmc');
+(1, 'bmc'),
+(2, 'Tour de Suisse');
 
-INSERT INTO fahrrad (id, fahrrad_type_id, marke_id) VALUES
-(1, 1, 1);
+INSERT INTO fahrrad_besitzer(id, name) VALUES
+(1, 'Hans'),
+(2, 'Sepp');
+
+INSERT INTO fahrrad (id, fahrrad_type_id, marke_id, fahrrad_besitzer_id) VALUES
+(1, 1, 1, 1),
+(2,2,2, 2);
 
