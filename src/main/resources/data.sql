ec0a93e1689bc7a1384e272ccfc6548c05d349d0
tobo3199 <tobias.sauter.003@outlook.com>
2022-07-15 11:44:19 +0200

    data sql statements hinzugefügt

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO fahrrad_type (id, typ) VALUES
+(1, 'bike');
+
+INSERT INTO marke (id, marken_type) VALUES
+(1, 'bmc');
+
+INSERT INTO fahrrad (id, fahrrad_type_id, marke_id) VALUES
+(1, 1, 1);
+
